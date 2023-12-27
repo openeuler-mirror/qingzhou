36c16efe0f1d5409fb417ef1d285ef2acf6057a2
wangpl <wangpl@tongtech.com>
2023-12-27 17:35:03 +0800

    移除暂不需要的代码

--- module/qingzhou-logger/src/main/java/qingzhou/logger/SystemPrintApiBackup.java
+++
@@ -1,8 +0,0 @@
-package qingzhou.logger;
-
-import java.io.PrintStream;
-
-public class SystemPrintApiBackup {
-    public static final PrintStream out = System.out;
-    public static final PrintStream err = System.err;
-}
--- module/qingzhou-logger/src/main/java/qingzhou/logger/impl/Controller.java
+++ module/qingzhou-logger/src/main/java/qingzhou/logger/impl/Controller.java
@@ -4,22 +4,12 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import qingzhou.logger.LoggerService;
-import qingzhou.logger.SystemPrintApiBackup;
 
 public class Controller implements BundleActivator {
     private ServiceRegistration<LoggerService> registerService;
 
     @Override
     public void start(BundleContext context) {
-        if (SystemPrintApiBackup.out == null) {
-            // 触发 ConsolePrintStream.out 的加载；
-            throw new IllegalStateException();
-        }
-        if (SystemPrintApiBackup.err == null) {
-            // 触发 ConsolePrintStream.err 的加载；
-            throw new IllegalStateException();
-        }
-
         registerService = context.registerService(LoggerService.class, new LoggerServiceImpl(), null);
     }
 
