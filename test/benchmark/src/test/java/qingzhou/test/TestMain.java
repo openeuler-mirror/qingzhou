685452264b913b6c640043ebbd38d15e243405aa
wangpl <wangpl@tongtech.com>
2024-01-01 12:12:11 +0800

    增加测试单元

---
+++ test/benchmark/src/test/java/qingzhou/test/TestMain.java
@@ -0,0 +1,11 @@
+package qingzhou.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestMain {
+    @Test
+    void test() {
+        Assert.assertTrue(true);
+    }
+}
