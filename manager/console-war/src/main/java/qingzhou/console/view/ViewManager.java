7e01147deb533aec17eba85ce78811ddd0a65b0d
微世界 <564818294@qq.com>
2024-05-27 18:20:11 +0800

    完善应用及实例功能

--- manager/console-war/src/main/java/qingzhou/console/view/ViewManager.java
+++ manager/console-war/src/main/java/qingzhou/console/view/ViewManager.java
@@ -33,9 +33,6 @@ public class ViewManager {
         // 完善响应的 msg
         if (response.getMsg() == null) {
             String appName = PageBackendService.getAppName(request);
-            if (DeployerConstants.INSTANCE_APP_NAME.equals(appName)) { // todo ？
-                appName = DeployerConstants.MASTER_APP_NAME;
-            }
             String SP = I18n.isZH() ? "" : " ";
             String msg = response.isSuccess() ? ConsoleI18n.getI18n(I18n.getI18nLang(), "msg.success") : ConsoleI18n.getI18n(I18n.getI18nLang(), "msg.fail");
             String model = I18n.getString(appName, "model." + request.getModel());
