d0eddf9aa9d62e29466f4597534e003d970f040d
微世界 <564818294@qq.com>
2024-06-05 10:40:56 +0800

    用户模块问题修复

--- module/qingzhou-config/src/main/java/qingzhou/config/User.java
+++ module/qingzhou-config/src/main/java/qingzhou/config/User.java
@@ -10,6 +10,21 @@ public class User {
     private String keyFor2FA;
     private String passwordLastModifiedTime;
 
+    private String digestAlg = "SHA-256";
+
+    private int saltLength = 4;
+
+    private int iterations = 5;
+    private boolean enablePasswordAge = true;
+
+    private int passwordMaxAge = 90;
+
+    private int passwordMinAge = 0;
+
+    private int limitRepeats = 5;
+
+    private String oldPasswords;
+
     public String getPasswordLastModifiedTime() {
         return passwordLastModifiedTime;
     }
@@ -72,5 +87,69 @@ public class User {
 
     public void setKeyFor2FA(String keyFor2FA) {
         this.keyFor2FA = keyFor2FA;
+    }
+
+    public String getDigestAlg() {
+        return digestAlg;
+    }
+
+    public void setDigestAlg(String digestAlg) {
+        this.digestAlg = digestAlg;
+    }
+
+    public int getSaltLength() {
+        return saltLength;
+    }
+
+    public void setSaltLength(int saltLength) {
+        this.saltLength = saltLength;
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    public boolean isEnablePasswordAge() {
+        return enablePasswordAge;
+    }
+
+    public void setEnablePasswordAge(boolean enablePasswordAge) {
+        this.enablePasswordAge = enablePasswordAge;
+    }
+
+    public int getPasswordMaxAge() {
+        return passwordMaxAge;
+    }
+
+    public void setPasswordMaxAge(int passwordMaxAge) {
+        this.passwordMaxAge = passwordMaxAge;
+    }
+
+    public int getPasswordMinAge() {
+        return passwordMinAge;
+    }
+
+    public void setPasswordMinAge(int passwordMinAge) {
+        this.passwordMinAge = passwordMinAge;
+    }
+
+    public int getLimitRepeats() {
+        return limitRepeats;
+    }
+
+    public void setLimitRepeats(int limitRepeats) {
+        this.limitRepeats = limitRepeats;
+    }
+
+    public String getOldPasswords() {
+        return oldPasswords;
+    }
+
+    public void setOldPasswords(String oldPasswords) {
+        this.oldPasswords = oldPasswords;
     }
 }
