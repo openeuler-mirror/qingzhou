285a272d679eab1bbb702052b583349fa6a47014
yyx <yuyx@tongtech.com>
2024-09-13 19:00:35 +0800

    完善测试用例，调整列表页“未查询到数据”图片位置

---
+++ apps/Example/src/main/java/qingzhou/app/model/PostManagementModel.java
@@ -0,0 +1,47 @@
+package qingzhou.app.model;
+
+import qingzhou.api.FieldType;
+import qingzhou.api.Model;
+import qingzhou.api.ModelField;
+import qingzhou.app.AddableModelBase;
+import qingzhou.app.ExampleMain;
+
+@Model(code = "postmanagement", icon = "stack",
+        menu = ExampleMain.SYSTEM_MANAGEMENT, order = 2,
+        name = {"岗位管理", "en:Post Management"},
+        info = {"岗位管理。", "en:Post management."})
+public class PostManagementModel extends AddableModelBase {
+    @ModelField(
+            list = true,
+            name = {"岗位名称", "en:Post Name"},
+            info = {"岗位名称。", "en:Post name."})
+    public String name;
+
+    @ModelField(
+            required = true,
+            list = true,
+            name = {"岗位编码", "en:Post Code"},
+            info = {"岗位编码。", "en:Post code."})
+    public String postCode;
+
+    @ModelField(
+            type = FieldType.radio,
+            options = {"正常", "停用"},
+            required = true,
+            list = true,
+            name = {"岗位状态", "en:Post Status"},
+            info = {"岗位状态。", "en:Post status."})
+    public String postStatus;
+
+    @ModelField(
+            type = FieldType.textarea,
+            list = true,
+            name = {"备注", "en:Notes"},
+            info = {"备注。", "en:Notes."})
+    public String notes;
+
+    @Override
+    public String idFieldName() {
+        return "name";
+    }
+}
