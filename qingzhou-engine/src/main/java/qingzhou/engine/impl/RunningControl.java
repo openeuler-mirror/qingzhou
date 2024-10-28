b6b4b223749affcdb892a17b93af35fb6f163c65
GlgjShsy_1s <weiyifan@isrc.iscas.ac.cn>
2024-10-28 17:37:52 +0800

    添加了检查重复启动的机制

--- qingzhou-engine/src/main/java/qingzhou/engine/impl/RunningControl.java
+++ qingzhou-engine/src/main/java/qingzhou/engine/impl/RunningControl.java
@@ -1,15 +1,14 @@
 package qingzhou.engine.impl;
 
-import java.io.*;
-import java.net.ConnectException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import qingzhou.engine.util.FileUtil;
 import qingzhou.engine.util.Utils;
 import qingzhou.engine.util.pattern.Process;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class RunningControl implements Process {
     private final EngineContext engineContext;
@@ -23,7 +22,7 @@ class RunningControl implements Process {
     public void exec() throws Exception {
         // 实例不可重复启动，因为端口和 temp 文件都会冲突
         running = FileUtil.newFile(engineContext.getInstanceDir(), "temp", "running");
-        if (checkService()) {
+        if (running.exists() && checkService()) {
             throw new IllegalStateException("Qingzhou is already starting");
         }
 
@@ -37,62 +36,43 @@ class RunningControl implements Process {
     }
 
     private boolean checkService() {
-        return checkPort()&&checkTmp();
+        return checkPort() && checkTmp();
     }
 
     private boolean checkPort() {
         try {
-            String jsonConfig=FileUtil.fileToString(FileUtil.newFile(engineContext.getInstanceDir(),"conf","qingzhou.json"));
+            String jsonConfig = FileUtil.fileToString(FileUtil.newFile(engineContext.getInstanceDir(), "conf", "qingzhou.json"));
             String regex = "\"port\":\\s*\"(\\d+)\"";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(jsonConfig);
-            String port="9000";
+            String port = "9000";
 
             if (matcher.find()) {
-                //String enabled = matcher.group(1); // 获取 enabled 的值
                 port = matcher.group(1); // 获取 port 的值
             } else {
                 System.out.println("未找到相关配置");
             }
-                URL url = new URL("http://localhost:"+port+"/console");
 
-                // 打开连接
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-                // 设置请求方法为GET
-                connection.setRequestMethod("GET");
+            try (Socket socket = new Socket("localhost", Integer.parseInt(port))) {
+                return true;
+                //System.out.println("Port " + finalPort + " is open");
+            } catch (IOException e) {
 
-                // 设置连接超时时间和读取超时时间（单位：毫秒）
-                connection.setConnectTimeout(1000); // 5秒
-                connection.setReadTimeout(1000); // 5秒
-
-            connection.disconnect();
-            int responseCode = connection.getResponseCode();
-
-                // 如果响应码是200（HTTP_OK），则表示连接成功
-                if (responseCode == 200) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return false;
             }
-        catch (ConnectException e){
-            return false;
+            // 端口关闭或无法连接
+        } catch (Exception e) {
+            throw new IllegalStateException("failed to check port!");
         }
-        catch (IOException e){
-            e.printStackTrace();
-            return false;
-        }
+
     }
-    private boolean checkTmp(){
+
+    private boolean checkTmp() {
         //检查当前是否存在临时文件
-        File temp=FileUtil.newFile(engineContext.getTemp());
-        //System.out.println(temp.getAbsolutePath());
-        //System.out.println(temp.exists());
-        //System.out.println(temp);
-        return temp.exists();}
-
-
+        File temp = FileUtil.newFile(engineContext.getTemp());
+        return temp.exists();
+    }
 
 
     @Override
