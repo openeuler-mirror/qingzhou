dc59fb147d8727df75861438866a56f6cf9f90cc
fengqiao <fengqiao@tongtech.com>
2024-11-20 15:30:12 +0800

    修复账户删除无弹窗问题

--- lib/manager/app-system/src/main/java/qingzhou/app/system/user/User.java
+++ lib/manager/app-system/src/main/java/qingzhou/app/system/user/User.java
@@ -222,6 +222,7 @@ public class User extends ModelBase implements General, Validate, Option {
     @ModelAction(
             code = Delete.ACTION_DELETE, icon = "trash",
             show = "name!=qingzhou",
+            action_type = ActionType.action_list,
             name = {"删除", "en:Delete"},
             info = {"删除本条数据，注：请谨慎操作，删除后不可恢复。",
                     "en:Delete this data, note: Please operate with caution, it cannot be restored after deletion."})
