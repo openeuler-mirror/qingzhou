99f1cfc2a6f35bd2a6f8383052a4fb7aa95520a0
hwxu <dev06106@linx-info.com>
2024-11-27 07:19:40 +0000

    update qingzhou-api/src/main/java/qingzhou/api/type/Option.java.

--- qingzhou-api/src/main/java/qingzhou/api/type/Option.java
+++ qingzhou-api/src/main/java/qingzhou/api/type/Option.java
@@ -25,7 +25,7 @@ public interface Option {
     String[] dynamicOptionFields();
 
     /**
-    * 根据字段名称，将选项数据组织成对应的页面表单字段项目并返回
+    * 根据字段名称，将选项数据组织成对应的页面表单字段项目
     *
     * @return 返回表单字段项目的数组，其中表单字段项目可包含名称或国际化信息数组
     */
