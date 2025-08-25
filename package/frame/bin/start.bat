dd1acd4d49396f63ee89bc9bff6aa9a3c03bb058
huanglei <huanglei@tongtech.com>
2025-08-25 16:03:17 +0800

    修复启动脚本问题

--- package/frame/bin/start.bat
+++ package/frame/bin/start.bat
@@ -8,7 +8,7 @@ if "%qingzhou_home%"=="" (
     set qingzhou_home=!qingzhou_home:~0,-1!
 )
 
-# 设定要启动的 instance
+rem 设定要启动的 instance
 if not "%~1"=="" (
     set runInstance=%~1
 ) else (
@@ -21,15 +21,15 @@ if not exist "%instanceDir%" (
     exit /b 1
 )
 
-# java 启动参数
+rem java 启动参数
 for /f "delims=" %%i in ('java -jar "%qingzhou_home%\bin\qingzhou-launcher.jar" start-args "%runInstance%" 2^>^&1') do set startCmd=%%i
 
-# 判断返回码，给出错误提示信息
+rem 判断返回码，给出错误提示信息
 if %errorlevel% neq 0 (
     echo !startCmd!
     exit /b %errorlevel%
 )
 
-# 设置工作目录，保障 qingzhou.json 里的 logs/jvm/jvm.log 能识别准确
+rem 设置工作目录，保障 qingzhou.json 里的 logs/jvm/jvm.log 能识别准确
 cd /d "%instanceDir%"
 %startCmd%
